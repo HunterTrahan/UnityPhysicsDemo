4d5fb336b0127761baf9707a0446485dd6e055d7
Hunter <trahan.hunter@yahoo.com>
2020-09-24 17:00:39 -0500

    Started on obstacle course

---
+++ Assets/_Scripts/RagdollTriggerBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollTriggerBehavior : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        RagdollBehavior ragdoll = collision.transform.GetComponentInParent<RagdollBehavior>();
+        if (ragdoll != null)
+            ragdoll.ragdollEnabled = true;
+    }
+}
